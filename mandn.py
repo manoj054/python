3324a942ac5476c94f135d1a979bdcf2dd270524
manojsunku <manojsunku2003@gmail.com>
2026-07-17 15:50:55 +0530

    hi

--- mandn.py
+++ mandn.py
@@ -1,5 +1,7 @@
 m = int(input("enter a number : "))
 n = int(input("enter a number : "))
-for i in range(m, n-1, -1):
+for i in range(m, n - 1, - 1):
+    print(i)
+for i in range(m, n + 1):
     print(i)
     
