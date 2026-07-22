7975f9a0f6ea955dfc2fa24791ba84a46dcfd733
manojsunku <manojsunku2003@gmail.com>
2026-07-22 10:19:12 +0530

    pull

---
+++ class.py
@@ -0,0 +1,5 @@
+class Class:
+    name = "Sunku Manoj Kumar"
+    age = 23
+print(Class.name)
+print(Class.age)
---
+++ student.py
@@ -0,0 +1,7 @@
+class student:
+    def __init__(self):
+        self.name = "Sunku Manoj kumar"
+        self.age = 23
+s1 = student()
+print(s1.name)
+print(s1.age)
